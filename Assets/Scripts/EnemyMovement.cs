c2a05a9bd7fa229186433ad221451366dc145039
I-AM-INSANE <vasiliyqqerty@gmail.com>
2021-08-09 17:29:12 +0300

    Pathfidning enemy update

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour
+{
+    #region Fields
+
+    [SerializeField] private NavMeshAgent agent;
+
+    private Transform player;
+
+    #endregion
+
+    #region Methods
+
+    private void Start()
+    {
+        player = FindObjectOfType<PlayerMovement>().transform;
+    }
+
+    private void FixedUpdate()
+    {
+        FollowPlayer();
+    }
+
+    private void FollowPlayer()
+    {
+        agent.SetDestination(player.position);
+    }
+
+    #endregion
+}
