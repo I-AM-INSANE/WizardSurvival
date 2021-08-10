bbd5b049578d6d55854294a3f82786a2cfd2d2d2
I-AM-INSANE <vasiliyqqerty@gmail.com>
2021-08-10 14:11:22 +0300

    Enemy spawn on point update

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    #region Fields
+
+    [SerializeField] private float cooldownBetweenSpawn;
+    [SerializeField] private GameObject[] enemies;
+
+    private GameObject[] spawnPoints;
+    private bool cooldown = false;
+
+    #endregion
+
+    #region Methods
+
+    private void Start()
+    {
+        spawnPoints = GameObject.FindGameObjectsWithTag("EnemySpawnPoint");
+    }
+
+    private void Update()
+    {
+        if (!cooldown)
+        {
+            SpawnEnemy(Random.Range(0, enemies.Length), Random.Range(0, spawnPoints.Length));
+            StartCoroutine(CooldownBetweenSpawnRoutine());
+        }
+    }
+
+    private void SpawnEnemy(int enemyNum, int pointNum)
+    {
+        Instantiate(enemies[enemyNum], spawnPoints[pointNum].transform.position, Quaternion.identity);
+    }
+
+    private IEnumerator CooldownBetweenSpawnRoutine()
+    {
+        cooldown = true;
+        yield return new WaitForSeconds(cooldownBetweenSpawn);
+        cooldown = false;
+    }
+
+    #endregion
+}
